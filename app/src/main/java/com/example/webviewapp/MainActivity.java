13d1d7f1e6c0fbc9f10a247dc289327562bcf51f
b21tilbe <b21tilbe@student.his.se>
2022-04-06 15:35:06 +0200

    Here I've created a private member variable. The website is "his.se".

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
@@ -28,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        webView = findViewById(R.id.web_view);
+
+        webView.loadUrl("https://his.se");
 
         /*
         * Rename your App. Tip: Values->Strings
